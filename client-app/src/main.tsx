65bc0809aabdf02326420a504916be7e30d416fb
yarenaktass <yarenaktasss20@gmail.com>
2024-01-29 20:15:53 +0300

    End of Section 5

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
 import 'semantic-ui-css/semantic.min.css'
+import App from './app/layout/App'
+import './app/layout/styles.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
