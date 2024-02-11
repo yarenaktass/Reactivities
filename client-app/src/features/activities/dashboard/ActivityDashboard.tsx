7aa1d5eab7ad6a521e6bb5a87c5017b3d93e26dc
yarenaktass <yarenaktasss20@gmail.com>
2024-02-11 20:18:17 +0300

    End of Section 6

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -14,13 +14,14 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting:boolean;
 }
 
-export default function ActivityDashboard({activities, selectedActivity, selectActivity,cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity }:Props) {
+export default function ActivityDashboard({activities, selectedActivity, selectActivity,cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity, submitting }:Props) {
   return (
     <Grid>
       <Grid.Column width="10">
-       <ActivityList activities={activities} selectActivity={selectActivity} deleteActivity={deleteActivity}/>
+       <ActivityList activities={activities} selectActivity={selectActivity} deleteActivity={deleteActivity} submitting={submitting}/>
       </Grid.Column>
       <Grid.Column width='6'>
         {selectedActivity && !editMode &&
@@ -30,7 +31,7 @@ export default function ActivityDashboard({activities, selectedActivity, selectA
         openForm={openForm}
          />}
          {editMode &&
-        <ActivityForm activity={selectedActivity} closeForm={closeForm} createOrEdit={createOrEdit} />}
+        <ActivityForm activity={selectedActivity} closeForm={closeForm} createOrEdit={createOrEdit} submitting={submitting} />}
       </Grid.Column>
     </Grid>
   );
