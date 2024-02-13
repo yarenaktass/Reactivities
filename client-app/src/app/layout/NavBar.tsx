ffd1d1b7ef98802df88ca58f5748b01abacebf1a
yarenaktass <yarenaktasss20@gmail.com>
2024-02-13 23:30:55 +0300

    End of Section 8

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,12 +1,12 @@
 import { Button, Container, Menu } from "semantic-ui-react";
-import { useStore } from "../stores/store";
+import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
-  const { activityStore } = useStore();
+
   return (
     <Menu inverted fixed="top">
       <Container>
-        <Menu.Item header>
+        <Menu.Item as={NavLink} to='/' header>
           <img
             src="/assets/logo.png"
             alt="logo"
@@ -14,10 +14,10 @@ export default function NavBar() {
           />
           Reactivities
         </Menu.Item>
-        <Menu.Item name="Activities" />
+        <Menu.Item as={NavLink} to='/activities' name="Activities" />
         <Menu.Item>
           <Button
-            onClick={() => activityStore.openForm()}
+           as={NavLink} to='/createActivity'
             positive
             content="Create Activity"
           />
--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,12 +1,13 @@
 import ReactDOM from 'react-dom/client'
 import 'semantic-ui-css/semantic.min.css'
-import App from './app/layout/App'
 import './app/layout/styles.css'
 import { StoreContext, store } from './app/stores/store'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './app/router/Routes'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <StoreContext.Provider value = {store}>
-        <App />
+        <RouterProvider router={router} />
     </StoreContext.Provider>
 )
